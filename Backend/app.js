e121a6563bfccde22aa7ac3034e9ae1336be9736
amir <mashhadifarahani1@gmail.com>
2023-11-22 19:12:42 +0330

    adding route for getting all items and single item

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,47 @@
 const express = require('express')
 const connect = require('./db/connect')
 require('dotenv').config()
+const Book =require('./models/Book')
 const app = express()
 
 
-app.get('/',(req,res)=>{
-    res.send('Home Page')
+
+app.get('/',async(req,res)=>{
+    try {
+        const books = await Book.find({})
+        return res.status(200).json({
+            count:books.length,
+            data:books
+        })
+    } catch (error) {
+        console.log(error);
+    }
 })
 
+app.post('/',async(req,res)=>{
+    try {
+        const book = await Book.create({
+           
+        })
+        res.status(201).send({book})
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.get('/:id',async(req,res)=>{
+    try {
+        const {id} = req.params
+        const singleBook = await Book.findById(id)
+        return res.status(200).json({
+            count:1,
+            data:singleBook
+        })
+        
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 const start = async()=>{
     try {
